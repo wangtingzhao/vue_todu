a02393b98e1dd46fd04ef2689b2d891f66f3b17c
wtz1990303 <wtz19930303@163.com>
2018-04-18 16:02:14 +0800

    todo学习

--- src/app.vue
+++ src/app.vue
@@ -2,18 +2,20 @@
 <div id="app">
     <div id="cover"></div>
     <Header></Header>
+    <Todo></Todo>
     <Footer></Footer>
 </div>
 </template>
 
 <script>
 import Header from './todo/header.vue'
-// import Footer from './todo/footer.jsx'
-
+import Footer from './todo/footer.jsx'
+import Todo from './todo/todo.vue'
 export default {
     components: {
         Header,
-        // Footer
+        Footer,
+        Todo
     }
 }
 </script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
 const config = {
+    target:'web',
     entry: path.join(__dirname,'src/index.js'),
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash:8].js',
         path: path.join(__dirname,'dist')
     },
     module:{
@@ -20,27 +22,13 @@ const config = {
                 test:/\.jsx$/,
                 loader: 'babel-loader' 
             },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.styl/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    'stylus-loader'
-                ]
-            },
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         'style-loader',
+            //         'css-loader'
+            //     ]
+            // },
             {
                 test: /\.(gif|jpg|jpeg|png|svg)$/,
                 use: [
@@ -66,12 +54,67 @@ const config = {
 }
 
 if (isDev) {
+    config.module.rules.push({
+        test: /\.styl/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: "postcss-loader",
+                options: {
+                    sourceMap: true
+                }
+            },
+            'stylus-loader'
+        ]
+    })
+  config.devtool = '#cheap-module-eval-source-map',
   config.devServer = {
    port: 8000,
    host: '0.0.0.0',
    overlay: {
      errors: true,
-   }
- }
+   },
+   hot: true
+//    historyFallback:{
+
+//    }
+//    open: true,
+  },
+  config.plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+  )
+} else {
+    config.entry = {
+        app: path.join(__dirname,'src/index.js'),
+        vendor: ['vue']
+    }
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push({
+        test: /\.styl/,
+        use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+                'css-loader',
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                'stylus-loader'
+            ]
+        })
+    })
+    config.plugins.push(
+        new ExtractPlugin('style.[contentHash:8].css'),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'runtime'
+        })
+    )
 }
 module.exports = config
